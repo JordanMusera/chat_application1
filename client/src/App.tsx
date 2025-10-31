feb6c01efd3974d8f082065f9b506d11a65fb81c
Jordan Musera <jordanmusera@gmail.com>
2025-10-31 13:58:10 +0300

    Add .gitignore and remove node_modules

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white gap-10">
+      <h1 className="text-4xl font-bold">Welcome to Simi online chat application</h1>
+      <button className="text-white rounded-xl bg-blue-400 p-3 text-xl">Get started</button>
+    </div>
+  );
+}
+
+export default App;
