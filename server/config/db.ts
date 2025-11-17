0f186558091950a8a3b7a3e36cb6251c11060f98
Jordan Musera <jordanmusera@gmail.com>
2025-11-17 20:11:01 +0300

    feat: configure database connection using environment variables

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,11 +1,13 @@
-import mysql from "mysql2/promise"
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"chat_app"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
 });
-
 
 export default db;
