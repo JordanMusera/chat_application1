004c2940a0b8c15bc6a11fc247b3d81378364111
Jordan Musera <jordanmusera@gmail.com>
2025-11-19 23:21:28 +0300

    Update groupRoute.ts

--- server/routes/groupRoute.ts
+++ server/routes/groupRoute.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import multer from "multer"
-import { createGroup, fetchGroups } from "../controllers/groupController";
+import { createGroup, fetchConvMessages, fetchGroups } from "../controllers/groupController";
 
 const router = express.Router();
 const upload = multer();
 
 router.post("/create_group",upload.single("avatar"),createGroup);
 router.get("/fetch_groups",fetchGroups);
+router.post("/fetch_messages",fetchConvMessages);
 
 export default router;
