00a141505de2cdc00e1077af35a0c96e86317622
Jordan Musera <jordanmusera@gmail.com>
2025-11-20 10:52:03 +0300

    Update mailService.ts

--- server/services/mailService.ts
+++ server/services/mailService.ts
@@ -6,7 +6,9 @@ dotenv.config();
 export const sendOTPToEmail = async (email: string, otp: string) => {
   try {
     const transport = nodemailer.createTransport({
-      service: "Gmail", 
+      secure:true,
+      host:'smtp.gmail.com',
+      port:465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
